2f1ea3bc3f444e1a08305d73e702b2f8f11e80c9
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-10-18 16:07:07 +0900

    플레이어 이동

--- 3DAction/Assets/Scripts/Player.cs
+++ 3DAction/Assets/Scripts/Player.cs
@@ -13,18 +13,29 @@ public class Player : MonoBehaviour
     Vector3 moveVec;
     [SerializeField]
     float speed;
-    void Start()
+    bool wDown;     //shift 키 인식
+
+    Animator anim;
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         hAxis = Input.GetAxisRaw("Horizontal"); //GetAxisRaw() : Axis 값을 정수로 변환하는 함수
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized; //normalized : 방향값이 1로 고정된 Vector
 
-        transform.position += moveVec * speed * Time.deltaTime; //transform의 이동은 deltaTime을 반드시 곱해주어야 함
+
+        transform.position += moveVec * speed  *(wDown ? 0.3f: 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero); 
+        anim.SetBool("isWalk",wDown);
+
+        //기본 회전 구현
+        transform.LookAt(transform.position + moveVec);
     }
 }
